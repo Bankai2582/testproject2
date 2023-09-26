a3fe888531ddd18699380b9236c36940ae7fda9b
Bankai2582 <soul2582@inbox.ru>
2023-09-26 19:12:49 +0300

    first practice lesson

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert('1');
+let numberOfFilms = +prompt('How many movies you watched?', '');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+
+let answer = prompt('What the last movie you watched?', '');
+let answer2 = prompt('How many points you give ?', '');
+let answer3 = prompt('What the last movie you watched?', '');
+let answer4 = prompt('How many points you give ?', '');
+
+personalMovieDB.movies[answer] = answer2;
+personalMovieDB.movies[answer3] = answer4;
+
+console.log(personalMovieDB);
