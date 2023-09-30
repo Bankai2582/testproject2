d42c9e9ed44e9e376b6e1675facbce4542709ade
Bankai2582 <soul2582@inbox.ru>
2023-09-30 04:58:39 +0300

    Practical lesson

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-let num =50;
+'use strict';
 
-// // while (num<55) {
-// //     console.log(num);
-// //     num++;
-// // }
+const numberOfFilms = +prompt('How many movies you watched?', '');
 
-// do {
-//     console.log(num);
-//     num++;
-// } while (num<55);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-for (let i=1; i<10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('What the last movie you watched?', ''),
+            b = prompt('How many points you give ?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
-   console.log(i);
 }
+
+if (personalMovieDB.count < 10) {
+    alert('You watched so little films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('You are classic viewer');
+} else if (personalMovieDB.count >= 30) {
+    alert('You are crazy!');
+} else {
+    alert('ERROR');
+}
+
+console.log(personalMovieDB);
+
