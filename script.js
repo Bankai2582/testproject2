580125c1bc7d1be2ee27091b8a2be7428ca17623
Bankai2582 <soul2582@inbox.ru>
2023-10-08 23:16:54 +0300

    Lesson 20 Object

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 'use strict';
 
-function first() {
-    // do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+// const obj = new Object();
 
-function second() {
-    console.log(2);
-}
+const option = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
 
-first();
-second();
+option.makeTest();
 
+const {border, bg} = option.colors;
+console.log(border);
 
-function learnJS(lang, callback) {
-    console.log(`I learn: ${lang}`);
-    callback();
-}
+// console.log(option['colors']['border']);
 
-learnJS('JavaScript', function() {
-    console.log('I was complited lessen!');
-});
+// delete option.name;
+
+// console.log(option);
+
+// let counter = 0;
+
+// for (let key in option) {
+//     if (typeof(option[key]) === 'object') {
+//         for (let i in option[key]) {
+//             console.log(`Object ${key} Property ${i} his value ${option[key][i]}`);
+//             // counter++;
+//         }
+//     } else {
+//         console.log(`Property ${key} his value ${option[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+console.log(Object.keys(option).length);
