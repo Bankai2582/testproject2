6db1482f241e2b6c1c284bb10502caefe9116a3b
Bankai2582 <soul2582@inbox.ru>
2023-10-08 22:26:30 +0300

    lesson 19 Callback function

--- script.js
+++ script.js
@@ -1,72 +1,25 @@
 'use strict';
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('How many movies you watched?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('How many movies you watched?', '');
-    }
-}
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-    const a = prompt('What the last movie you watched?', ''),
-            b = prompt('How many points you give ?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }
-}
-}
-// rememberMyFilms();
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-    alert('You watched so little films');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert('You are classic viewer');
-} else if (personalMovieDB.count >= 30) {
-    alert('You are crazy!');
-} else {
-    alert('ERROR');
+function first() {
+    // do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
 
-}
-// detectPersonalLevel();
-
-
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
+function second() {
+    console.log(2);
 }
 
-showMyDB(personalMovieDB.privat);
+first();
+second();
 
-function writeYourGengers() {
-    for (let i = 1; i <= 3 ; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Your favorite genres ${i}`);
-    }
+
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
 }
-writeYourGengers();
 
+learnJS('JavaScript', function() {
+    console.log('I was complited lessen!');
+});
