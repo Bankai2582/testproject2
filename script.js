8b5e0edf8b74e519e21ddb5b0c66e1e2544a64d9
Bankai2582 <soul2582@inbox.ru>
2023-10-14 00:21:56 +0300

    lessom 25 Dinamic typeof

--- script.js
+++ script.js
@@ -1,80 +1,47 @@
 'use strict';
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function() {
-        personalMovieDB.count = +prompt('How many movies you watched?', '');
+// To String
 
-    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-        personalMovieDB.count = +prompt('How many movies you watched?', '');
-    }},
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+console.log(String(4));
 
-    rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-        const a = prompt('What the last movie you watched?', ''),
-                b = prompt('How many points you give ?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }},
-
-    detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-        alert('You watched so little films');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert('You are classic viewer');
-    } else if (personalMovieDB.count >= 30) {
-        alert('You are crazy!');
-    } else {
-        alert('ERROR');
-    }},
-
-    showMyDB: function(hidden) {
-        if (!hidden) {
-        console.log(personalMovieDB);
-    }},
-
-    writeYourGengres: function() {
-        for (let i = 1; i < 2 ; i++) {
-            // let genre = prompt(`Your favorite genres ${i}`);
-
-            // if (genre == '' || genre == null) {
-            //     console.log('You write not corrent data');
-            //     i--;
-            // } else {
-            //     personalMovieDB.genres[i - 1] =  genre;
-            // }
-
-            let genres = prompt(`Your favorite genres`).toLowerCase();
-            if (genres == '' || genres == null) {
-                    console.log('You write not corrent data');
-                    i--;
-                } else {
-                    personalMovieDB.genres = genres.split(', ');
-                    personalMovieDB.genres.sort();
-                }
-
-    } personalMovieDB.genres.forEach((item, i) => {
-        console.log(`Любимый жанр ${i + 1} - это ${item}`);
-    })
-},
-
-    toggleVisibleMyDB: function() {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = false;
-        } else {
-            personalMovieDB.privat = true;
-        }
-    }
+console.log(typeof( + ''));
 
 
-};
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+// TO Number
+// 1
+console.log(typeof(Number('4')));
+// 2
+console.log(typeof(+'4'));
+// 3
+console.log(typeof(parseInt('14px', 10)));
+///
+
+let answ = +prompt('Hello', '');
+
+// To boolean
+//0,'', null, undefined, NaN; - FALSE
+//1
+let switcher = null;
+
+if(switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if(switcher) {
+    console.log('Working...');
+}
+
+//2
+console.log(typeof(Boolean('4')));
+//3
+console.log(typeof(!!'444'));
