e8859e0eb10ca9dd8230795728b2bd8b07af1857
Bankai2582 <soul2582@inbox.ru>
2023-10-14 17:47:50 +0300

    Lesson 29 use and chanced elements HTML

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 'use strict';
 
 const box = document.getElementById('box');
-
-console.log(box);
-
 // const btns = document.getElementsByTagName('button')[1];
-
 const btns = document.getElementsByTagName('button');
-
-console.log((btns[1]));
-
 const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart')
-hearts.forEach(item => {
-    console.log(item);
-});
-console.log(hearts);
-
+const hearts = document.querySelectorAll('.heart');
+const wrapper = document.querySelector('.wrapper');
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+// console.log(hearts);
 const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = 'background-color: yellow; width: 600px';
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+// for (let i=0; i<hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'black';
+// }
+
+hearts.forEach(item => {
+    item.style.backgroundColor = 'yellow';
+})
+
+const div = document.createElement('div');
+const text = document.createTextNode('I wam being here');
+
+div.classList.add('black');
+
+// document.querySelector('.wrapper').append(div);
+
+// wrapper.append(div);
+
+wrapper.prepend(div);
+
+// hearts[0].after(div);
+
+// circles[0].remove();
+
+// hearts[0].replaceWith(circles[0]);
+
+div.innerHTML = '<h1>Hello World</h1>';
+
