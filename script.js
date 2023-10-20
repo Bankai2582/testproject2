a873158f09edd38495528932385cbe488c637173
Bankai2582 <soul2582@inbox.ru>
2023-10-20 13:44:02 +0300

    lesson 35 defer async and dinamic criate script

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
 'use strict';
 
-// touchstart  
-// touchmove
-// touchend
-// touchenter
-// touchleave
-// touchcancel
-
-// Доп.события 
-// touches
-// targetTouches
-// changedTouches
-
-window.addEventListener('DOMContentLoaded', () => {
-        const box = document.querySelector('.box');
-
-        box.addEventListener('touchstart', (e) => {
-            e.preventDefault();
-
-            console.log('Start');
-            console.log(e.targetTouches);
-
-        });
-
-        box.addEventListener('touchmove', (e) => {
-            e.preventDefault();
-
-            console.log(e.targetTouches[0].pageX);
-        });
-
-        box.addEventListener('touchend', (e) => {
-            e.preventDefault();
-
-            console.log('ENd');
-        });
+const p = document.querySelectorAll('p');
+console.log(p);
 
 
 
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src; 
+    script.async = false;
+    document.body.append(script);
+}
 
-
-});
-//asd
+loadScript('test.js');
+loadScript('some.js');
