6890a33f5e2f078800b4028f44f2cc7969c5dacb
Bankai2582 <soul2582@inbox.ru>
2023-10-21 19:20:16 +0300

    Lesson 37 ClassList and delegironet

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 'use strict';
 
-const p = document.querySelectorAll('p');
-console.log(p);
+const btns = document.querySelectorAll('button'),
+        wrapper = document.querySelector('.btn-block');
 
 
+// // //console.log(btns[0].classList.length);
+// // console.log(btns[0].classList.item(0));
+// console.log(btns[1].classList.add('red', 'tratatatara'));
+// console.log(btns[3].classList.add('blue'));
+// // // console.log(btns[0].classList.remove('blue'));
+// // // console.log(btns[0].classList.toggle('blue'));
 
-function loadScript(src) {
-    const script = document.createElement('script');
-    script.src = src; 
-    script.async = false;
-    document.body.append(script);
-}
+// // if (btns[1].classList.contains('red')) {
+// //     console.log('red');
+// // }
 
-loadScript('test.js');
-loadScript('some.js');
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')) {
+    //     btns[1].classList.add('red');
+    // } else {
+    //     btns[1].classList.remove('red');
+    // }
+    // === 
+    btns[1].classList.toggle('red');
+})
+
+wrapper.addEventListener('click', (e) => {
+    // console.dir(e.target);
+    if (e.target && e.target.matches('button.red')) {
+        console.log('Hello');
+    }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
