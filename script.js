879da6f0f413debd15b0a7dcfb4c17fe362fd9ba
Bankai2582 <soul2582@inbox.ru>
2023-10-27 15:45:05 +0300

    Lesson 46 this

--- script.js
+++ script.js
@@ -1,23 +1,83 @@
 'use strict';
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`)
-    };
-}
 
-User.prototype.exit = function(name) {
-    console.log(`User ${this.name} is exit`)
-}
 
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 33);
 
-ivan.hello();
-alex.exit();
+// 1) Usualy function 'this' = window, 'use strict' is turn off,
+// but if 'use strict' is turn on then his score ths = undefined/
 
-// console.log(ivan);
-// console.log(alex);
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+// showThis(4, 5);
+
+// 2) If we use a function like a object method then 'this' = obj
+
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         function shout() {
+//             console.log(this)
+//         }
+//         shout();
+//     }
+// };
+// obj.sum();
+
+// 3) this в конструкторах и классах - это новый экземпляр объекта.
+
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+// }
+// let ivan = new User('Ivan', 23);
+
+// 4) Ручная привязка this: call, apply, bind
+
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// const user = {
+//     name: 'John'
+// };
+
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(13));
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function() {
+    // console.log(this);
+    this.style.backgroundColor = 'red';
+})
+
+const obj = {
+    num: 5,
+    sayNumber: function() {
+        const say = () => {
+            console.log(this.num)
+        };
+        say();
+    }
+};
+
+obj.sayNumber();
+
+const double = a => a * 2;
