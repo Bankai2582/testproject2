20c56f6e960dfd901a938fb4c92ba76c73586418
Bankai2582 <soul2582@inbox.ru>
2023-11-03 17:53:40 +0300

    Lesson 52 XMLHttpRequest Server and send

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 'use strict';
 
 
-const person = {
-   name: 'Alex',
-   tel: '+79913123123',
-   perant: {
-      mom: 'Olga',
-      dad: 'Mike'
-   }
- 
-};
+const inputRub = document.querySelector('#rub');
+const inputUsd = document.querySelector('#usd');
 
-const clone = JSON.parse(JSON.stringify(person));
-clone.perant.mom = 'Anna';
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
 
-console.log(clone);
-console.log(person);
+    request.open('GET', 'current.json');
+    request.setRequestHeader('Content-type', 'apllication/json; charset=utf-8')
+    request.send();
+
+    // События
+    // request.addEventListener('readystatechange', () => {
+    //     if (request.readyState === 4 && request.status === 200) {
+    //         console.log(request.response);
+    //         const data = JSON.parse(request.response);
+    //         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+    //     } else {
+    //         inputUsd.value = "Что-то пошло не так";
+    //     }
+    // });
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = "Что-то пошло не так";
+        }
+    });
+
+
+    // Свойства
+
+    // status
+    // statusText
+    // response
+    // readyState
+    
+})
